fabd8ddf93e26af5d2b7833d5660abed2a89dedf
Nileshgiri7 <90551714+Nileshgiri7@users.noreply.github.com>
2024-08-29 15:27:05 +0530

    tst

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Nilesh Giri JI</span>
+            Hi, I am <span className="text-[#915EFF]">Nilesh Giri JI</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Software developer,<br className="sm:block hidden" />
